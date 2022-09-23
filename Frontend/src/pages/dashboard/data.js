1703b09e07718b00c76e8e7cad95999072895536
KazePhan <thanhphanqwe@gmail.com>
2022-09-23 23:13:36 +0700

    first commit

---
+++ Frontend/src/pages/dashboard/data.js
@@ -0,0 +1,12 @@
+export const useData=[
+    {
+        id:1,
+        name:'John Doe',
+        email:'',
+        phone:'',
+        address:'',
+        city:'',
+        state:'',
+        zip:'',
+        country:'',
+]
